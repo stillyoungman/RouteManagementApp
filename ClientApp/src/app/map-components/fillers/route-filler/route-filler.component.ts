9e3ac942ca7a6dcc51f333701091dacede5029f5
Constantine Nikolsky <myheavybox@gmail.com>
2018-10-09 22:14:31 +0300

    Implemented saving of route

--- ClientApp/src/app/map-components/fillers/route-filler/route-filler.component.ts
+++ ClientApp/src/app/map-components/fillers/route-filler/route-filler.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { WebApiService } from '../../../core/services/web-api.service';
+import { ApplicationService } from 'src/app/core/services/application.service';
 
 @Component({
   selector: 'route-filler',
@@ -10,7 +11,7 @@ export class RouteFillerComponent implements OnInit {
 
   @Input() Route;
 
-  constructor(private api:WebApiService) { }
+  constructor(private api:WebApiService, private app: ApplicationService) { }
 
   ngOnInit() {
   }
@@ -18,7 +19,7 @@ export class RouteFillerComponent implements OnInit {
   save(){
     console.log("Saving");
     this.api.saveRoute(this.Route).subscribe( res => {
-      console.log(res.json());
+      this.app.redirectTo("my-routes");
     }, err => {
       console.log("ERR", err); 
     });
