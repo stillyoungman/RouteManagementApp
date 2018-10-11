5266105113c93eea1e4b7f8dfd9db6525e3d6d77
Constantine Nikolsky <myheavybox@gmail.com>
2018-10-11 21:27:16 +0300

    feature Saving from local storage if no authenticated

--- ClientApp/src/app/core/services/auth.service.ts
+++ ClientApp/src/app/core/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Headers } from '@angular/http';
 import { WebApiService } from './web-api.service';
 import { ApplicationService } from './application.service';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,9 @@ export class AuthService {
   errorMessage;
   claims;
 
-  constructor(private api: WebApiService, private app: ApplicationService) {
+  constructor(private api: WebApiService, 
+      private app: ApplicationService,
+      private notiService: NotificationService) {
     if (!!localStorage.getItem('t')) {
       // this._isAuthenticated = true;
       this.claims = this.tokenClaims;
@@ -34,7 +37,8 @@ export class AuthService {
   authenticate(loginUserDto: { email: string, password: string }) {
     this.api.authenticate(loginUserDto).subscribe(res => {
       localStorage.setItem('t', res.json().access_token);
-      this.app.router.navigate(["create-route"]);
+      
+      this.saveFromLocalStorate();
 
       this._isAuthenticated = true;
       this.claims = this.tokenClaims;
@@ -56,5 +60,20 @@ export class AuthService {
   logout() {
     this._isAuthenticated = false;
     localStorage.removeItem('t');
+  }
+
+  private saveFromLocalStorate(){
+    if(sessionStorage.getItem('toSave')){
+      this.api.saveRoute(JSON.parse(sessionStorage.getItem('toSave')))
+        .subscribe( res => {
+            this.app.redirectTo('my-routes');
+        }, err => { 
+          this.app.redirectTo('')
+        }, () => {
+          sessionStorage.removeItem('toSave');
+        });
+    } else {
+      this.app.router.navigate(["create-route"]);
+    }
   }
 }
