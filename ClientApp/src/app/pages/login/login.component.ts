2175182bd9c7404903dbea8177047547cf3ab26a
Constantine Nikolsky <myheavybox@gmail.com>
2018-10-21 20:50:02 +0300

    implemented login page validation

--- ClientApp/src/app/pages/login/login.component.ts
+++ ClientApp/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApplicationService } from '../../core/services/application.service';
 import { AuthService } from '../../core/services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,10 +13,18 @@ export class LoginComponent implements OnInit {
 
   email;
   password;
+  authForm:FormGroup;
+  loading;
 
-  constructor(private app: ApplicationService, private authService: AuthService) { }
+  constructor(private app: ApplicationService, 
+    private authService: AuthService) { }
 
   ngOnInit() {
+    this.authForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    }
+    )
   }
 
   cancel(){
@@ -23,7 +32,24 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
+    this.loading = true;
     this.authService.authenticate({email:this.email, password: this.password});
+  }
+
+  get errorMessage(){
+    return this.authService.errorMessage || 
+      (this.authForm.controls.email.touched &&
+        this.authForm.controls.email.errors &&
+        this.authForm.controls.email.errors.required && 
+        "Email is required...") ||
+      (this.authForm.controls.email.touched && 
+        this.authForm.controls.email.errors &&
+        this.authForm.controls.email.errors.email &&
+        "Incorrect email...") || 
+      (this.authForm.controls.password.touched && 
+        this.authForm.controls.password.errors &&
+        this.authForm.controls.password.errors.required && 
+        "Password is required...")
   }
 
 }
